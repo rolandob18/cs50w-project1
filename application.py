b6f2a8a4389c7145cc7c208df4190d460b13f699
rolandob18 <100975055+rolandob18@users.noreply.github.com>
2022-09-30 15:58:36 -0600

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,4 @@
-#from crypt import methods
-#from email.mime import application
 import os
-import re
-from click import password_option
 
 from flask import flash,redirect, render_template, Flask, request, session
 from flask_session import Session
@@ -14,8 +10,8 @@ from helpers import login_required, apology
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-   raise RuntimeError("DATABASE_URL is not set")
+#if not os.getenv("DATABASE_URL"):
+#   raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -23,7 +19,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine("postgresql://setruvhdhepzne:14dde31f92e7cfd1bed1083f125402dc352cd503bc851057fccddafca4b5c551@ec2-34-227-135-211.compute-1.amazonaws.com:5432/dd7jm4tkdkdbed")
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
@@ -42,7 +38,7 @@ def login():
             flash("Ingrese una contraseña")
             return render_template("login.html")
 
-        consulta = db.execute("SELECT * FROM username = ?", request.form.get("username"))
+        consulta = db.execute("SELECT * FROM usuarios = ?", request.form.get("username"))
         
         if len(consulta) != 1 or not check_password_hash(consulta[0]["password"], request.form.get("password")):
             flash("El usuario no existe o la contraseña no es correcta")
@@ -67,7 +63,7 @@ def register():
         confirmation = request.form.get("confirmation")
 
         if not username:
-            flash("Ingrese un nomre")
+            flash("Ingrese un nombre")
             return render_template("register.html")
         if not password:
             flash("Ingrese una contrasña")
@@ -78,16 +74,19 @@ def register():
 
         hash = generate_password_hash(password)
 
-        consulta = db.execute("SELECT * FROM usuarios WHERE username = ?", username)
+        consulta = db.execute("SELECT * FROM usuarios WHERE username = :username", {
+            "username": username
+        }).fetchone()
 
-        if len(consulta) >=1 :
-            flash("username already exists")
+        if consulta :
+            flash("Ese nombre ya existe")
             return render_template("register.html")
 
-        nuevo_usuario = db.execute(f"INSERT INTO usuarios (username, password) VALUES(?,?)", username, hash)    
+        nuevo_usuario = db.execute(f"INSERT INTO usuarios (username, password) VALUES(:username, :password) RETURNING id", {"username": username, "password": password}).fetchone()
 
+    print(nuevo_usuario)
     session["user_id"] = nuevo_usuario
-    print(session["user_id"])
+    db.commit()
     return redirect("/")
 
 @app.route("/logout")
